c40f64c979c889a8affb10292049e2f05fce70db
EgorPash <2021104302@pnu.edu.ru>
2024-03-15 01:17:47 +1000

    fourth commit

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ def add(a, b):
   """
 
   return a + b
+
+def subtract(a, b):
+  """
+  Вычитает второе число из первого.
+
+  Args:
+    a (int): Первое число.
+    b (int): Второе число.
+
+  Returns:
+    int: Разность двух чисел.
+  """
+
+  return a - b
